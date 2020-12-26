71b0f9453f34877168e3726ccb031086dc1b3b29
salni84 <roberto.salerno@swissscom.com>
2020-12-26 10:48:33 +0100

    update travis

--- Poker-Frontend/karma.conf.js
+++ Poker-Frontend/karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function (config) {
       require('karma-firefox-launcher'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-phantomjs-launcher')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
