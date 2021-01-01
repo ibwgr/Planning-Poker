88a433a71fe844307b4b82d7374a424be2ff6618
salni84 <roberto.salerno@swissscom.com>
2021-01-01 12:04:14 +0100

    update tests

--- Poker-Frontend/src/app/connection.service.spec.ts
+++ Poker-Frontend/src/app/connection.service.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-
+import {async, TestBed, tick} from '@angular/core/testing';
 import { ConnectionService } from './connection.service';
 
-describe('ConnectionService', () => {
+
+fdescribe('ConnectionService', () => {
   let service: ConnectionService;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ConnectionService);
+    service.connection.next({
+      type: "message", text: "ping"
+    });
+    service.connection.complete()
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should check if websocket-server is available', async (() => {
+
+    service.connection.subscribe((data) => {
+      JSON.parse(data).then(()=> {
+        expect(data.text).toBe('ping')
+      })
+    });
+
+
+
+
+
+  }));
 });
