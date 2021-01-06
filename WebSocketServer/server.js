08e05d050f66a971e15e0d84f702549c219d3d7d
Sebastian Rüegg <sebastian.rueegg@bluewin.ch>
2021-01-06 10:01:02 +0100

    WebSocketServer sends ping
    - sends pings every 20 seconds to keep the connection alive

--- WebSocketServer/server.js
+++ WebSocketServer/server.js
@@ -8,7 +8,18 @@ const webSocketServer = new WebSocket.Server({ server: server });
 
 app.use(express.static(__dirname + "/"));
 
+function dummy(){};
+
+function heartbeat() {
+    this.isAlive = true;
+    console.log("Still connected")
+}
+
 webSocketServer.on('connection', (webSocketClient) => {
+    console.log('Websocket Server connected to a new client')
+    webSocketClient.isAlive = true;
+    webSocketClient.on('pong', heartbeat)
+
     webSocketClient.on('message', (message) => {
         webSocketServer.clients.forEach((client) => {
             if (client !== webSocketClient && client.readyState === WebSocket.OPEN) {
@@ -17,6 +28,21 @@ webSocketServer.on('connection', (webSocketClient) => {
         })
     })
 });
+
+const interval = setInterval(function ping(){
+    webSocketServer.clients.forEach(function each(ws){
+        if (ws.isAlive === false) {
+            return ws.terminate();
+        }
+            ws.isAlive = false;
+            ws.ping(dummy);
+    });
+}, 20000);
+
+webSocketServer.on('close', function close(){
+    console.log("Websocket Server closed.");
+    clearInterval(interval);
+})
 
 server.listen(port, () => {
     console.log(`Server is listening on ${port}!`)
