33a07c047fbcfcc136b551475da6af7940aa49e5
Sebastian Rüegg <sebastian.rueegg@bluewin.ch>
2021-01-08 12:45:20 +0100

    Websocket Server and Average calculation
    - Websocket Server uses cleaned up code
    - Estimations component shows average without rounding

--- Poker-Frontend/src/app/estimations/estimations.component.spec.ts
+++ Poker-Frontend/src/app/estimations/estimations.component.spec.ts
@@ -36,9 +36,14 @@ describe('EstimationsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should calculate right average',  () => {
-    const votes = [2, 3, 5, 0, 1];
-    expect(component.calcAverage(votes)).toEqual(3);
+  it('should calculate right average int',  () => {
+    const votes = [5,5,5];
+    expect(component.calcAverage(votes)).toEqual(5);
+  });
+
+  it('should calculate right average float',  () => {
+    const votes = [5,3,2];
+    expect(component.calcAverage(votes)).toEqual(3.33);
   });
 
   it('should calculate the right sum', () => {
